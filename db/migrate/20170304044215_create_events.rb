e05409c5e60e7165dcbaa7bec9ae06e005174247
kdavis99 <kdavis99@mail.csuchico.edu>
2017-03-04 09:44:50 -0800

    added maps to the homepage with the events showing on them

--- db/migrate/20170304044215_create_events.rb
+++ db/migrate/20170304044215_create_events.rb
@@ -1,14 +1,16 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
-  def change
-    create_table :events do |t|
-      t.datetime :s_time
-      t.datetime :e_time
-      t.string :name
-      t.string :address
-      t.string :note
-      t.references :user, foreign_key: true
+   def change
+      create_table :events do |t|
+         t.datetime :s_time
+         t.datetime :e_time
+         t.string :name
+         t.string :address
+         t.string :note
+         t.float :latitude
+         t.float :longitude
+         t.references :user, foreign_key: true
 
-      t.timestamps
-    end
-  end
+         t.timestamps
+      end
+   end
 end
